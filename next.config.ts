4ea04b3ed6a63741b14ced07fa6f0ea26e838b00
とんさん <pinokio.tomo@gmail.com>
2025-10-22 17:50:00 +0900

    chore(dev): use port 3000 in dev and trim allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -9,11 +9,7 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   experimental: {
-    allowedDevOrigins: [
-      "http://localhost:3001",
-      "http://192.168.2.7:3001",
-      "http://192.168.2.7:3000",
-    ],
+    allowedDevOrigins: ["http://localhost:3000","http://192.168.2.7:3000"],
   },
 }
 
