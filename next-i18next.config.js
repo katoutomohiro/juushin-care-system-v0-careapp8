93a7b98ff2224d6c99579fad43494fba5baf3c97
とんさん <pinokio.tomo@gmail.com>
2025-10-23 17:08:25 +0900

    fix i18n config for build pass

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,9 +1,7 @@
-/** @type {import('next-i18next').UserConfig} */
 module.exports = {
   i18n: {
     defaultLocale: 'ja',
     locales: ['ja', 'en'],
   },
-  localePath: typeof window === 'undefined' ? require('path').resolve('./public/locales') : '/locales',
   reloadOnPrerender: process.env.NODE_ENV === 'development',
 }
