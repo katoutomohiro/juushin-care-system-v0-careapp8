68beda8a6002663e39bde6ded3dded33ff501594
とんさん <pinokio.tomo@gmail.com>
2026-01-07 09:29:39 +0900

    chore: remove unused vars in next.config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next"
 
-const DEV_PORT = 3000
-const ip = process.env.DEV_HOST
-
 const nextConfig: NextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: {
