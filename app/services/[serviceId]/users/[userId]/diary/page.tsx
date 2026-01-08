913b1b7c78a1668c0598dd0b8456d1439a145b01
とんさん <pinokio.tomo@gmail.com>
2026-01-08 09:33:35 +0900

    1/8

--- app/services/[serviceId]/users/[userId]/diary/page.tsx
+++ app/services/[serviceId]/users/[userId]/diary/page.tsx
@@ -1,12 +1,15 @@
 'use client'
 import { use } from 'react'
+import { useSearchParams } from 'next/navigation'
 import CommonDiary from '@/components/diary/CommonDiary'
 
 export default function ServiceUserDiaryPage({ params }: { params: Promise<{ serviceId: string; userId: string }> }) {
   const { serviceId, userId } = use(params)
+  const search = useSearchParams()
+  const careReceiverId = search.get('careReceiverId') ?? undefined
   return (
     <div className="p-4">
-      <CommonDiary serviceId={serviceId} userId={userId} />
+      <CommonDiary serviceId={serviceId} userId={userId} careReceiverId={careReceiverId} />
     </div>
   )
 }
